4be2e8ccd9335f71eaa79a726a48c175237fe8f1
north-tower <muneshmyke@gmail.com>
2024-07-18 15:30:22 +0300

    57

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import Image from "next/image";
 
 function Logo(props: any) {
 
@@ -7,7 +7,8 @@ const { renderDefault, title } = props;
   return (
     <div className="flex items-center space-x-2">
     
-   <Image className="rounded-full object-cover"
+   <Image 
+   className="rounded-full object-cover"
    height={50}
    width={50}
    src="https://i.postimg.cc/KjpHL5Cm/Whats-App-Image-2024-07-11-at-16-50-51-1.jpg"
