536747ec74413fb68df33b5a9492a9f0df1db71a
north-tower <muneshmyke@gmail.com>
2024-07-19 16:21:47 +0300

    107

--- components/Carousel2.tsx
+++ components/Carousel2.tsx
@@ -7,8 +7,36 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { groq } from "next-sanity";
+import { client, urlFor } from "@/lib/sanity.client";
+
+
+
+const query = groq`
+*[_type == "clients"]
+`;
+
+interface clients {
+  image: string;
+}
 
 export function Carousel2() {
+  const [images, setImages] = React.useState<clients[]>([]);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const initialImage = await client.fetch(query);
+        console.log("Fetched image:", initialImage);
+        setImages(initialImage);
+      } catch (error) {
+        console.error("Failed to fetch posts:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   // Example usage of Carousel2 with dynamic data
   const data = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5"];
 
@@ -20,13 +48,13 @@ export function Carousel2() {
       className="w-full max-w-sm "
     >
       <CarouselContent className="">
-        {data.map((item, index) => (
+        {images.map((mainImage, index) => (
           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
             <div className="p-1 ">
               <Card className="rounded-2xl">
                 <CardContent className="flex items-center justify-center p-3 " style={{ height: '90px' }}>
                   <img
-                    src="https://assets-global.website-files.com/6009ec8cda7f305645c9d91b/601074ed0f05cd25097215a4_6002086f72b7277e1f01d682_ryan-morrison-illustration-1.png"
+                    src={urlFor(mainImage).url()}
                     alt=""
                     className="h-full w-full object-contain"
                   />
--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,4 +1,5 @@
 import { createClient } from "next-sanity";
+import createImageUrlBuilder from "@sanity/image-url"
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
@@ -10,3 +11,6 @@ export const client = createClient({
     apiVersion,
     useCdn: false,
 })
+
+export const urlFor = (source : any) => 
+    createImageUrlBuilder(client).image(source)
