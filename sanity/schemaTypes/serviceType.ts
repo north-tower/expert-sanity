aebb051e34f39a67f998cfbd7d55289259fada2e
north-tower <muneshmyke@gmail.com>
2024-07-20 16:52:33 +0300

    550552

--- sanity/schemaTypes/serviceType.ts
+++ sanity/schemaTypes/serviceType.ts
@@ -12,41 +12,85 @@ export const serviceType = defineType({
       validation: Rule => Rule.required().error('Title is required'),
     }),
     defineField({
-        name: 'description',
-        title: 'Description',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
-      defineField({
-        name: 'href',
-        title: 'href',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
-      defineField({
-        name: 'step1',
-        title: 'step1',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
-      defineField({
-        name: 'step2',
-        title: 'step2',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
-      defineField({
-        name: 'step3',
-        title: 'step3',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
-      defineField({
-        name: 'step4',
-        title: 'step4',
-        type: 'string',
-        validation: Rule => Rule.required().error('Description is required'),
-      }),
+      name: 'description',
+      title: 'Description',
+      type: 'string',
+      validation: Rule => Rule.required().error('Description is required'),
+    }),
+    defineField({
+      name: 'href',
+      title: 'href',
+      type: 'string',
+      validation: Rule => Rule.required().error('Description is required'),
+    }),
+    defineField({
+      name: 'step1',
+      title: 'Step 1',
+      type: 'object',  // Changed type to 'object'
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+        },
+        {
+          name: 'description',  // New field added
+          type: 'string',
+          title: 'Description',
+        }
+      ]
+    }),
+    defineField({
+      name: 'step2',
+      title: 'Step 2',
+      type: 'object',  // Changed type to 'object'
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+        },
+        {
+          name: 'description',  // New field added
+          type: 'string',
+          title: 'Description',
+        }
+      ]
+    }),
+    defineField({
+      name: 'step3',
+      title: 'Step 3',
+      type: 'object',  // Changed type to 'object'
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+        },
+        {
+          name: 'description',  // New field added
+          type: 'string',
+          title: 'Description',
+        }
+      ]
+    }),
+    defineField({
+      name: 'step4',
+      title: 'Step 4',
+      type: 'object',  // Changed type to 'object'
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+        },
+        {
+          name: 'description',  // New field added
+          type: 'string',
+          title: 'Description',
+        }
+      ]
+    }),
   ],
   preview: {
     select: {
